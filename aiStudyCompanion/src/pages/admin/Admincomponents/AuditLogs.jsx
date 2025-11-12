2cd06b1f42e335b019ed0a91df57a7f17a5dc1ac
Just-Learning-here <108979880+Just-Learning-here@users.noreply.github.com>
2025-11-13 01:35:01 +0300

    almost done with admin dashboard

---
+++ aiStudyCompanion/src/pages/admin/Admincomponents/AuditLogs.jsx
@@ -0,0 +1,40 @@
+
+
+function AuditLogs() {
+        return(
+            <>
+            <header className="flex flex-row  p-6 m-6 border rounded-lg shadow-lg w-full max-w-md">
+                <h1 className="w-full">Audit Logs Panel</h1>
+                <section className="flex flex-col p-2">
+                
+                <label htmlFor="date">Filter by Date</label>
+                    <select name="date" id="date" className="p-2 border border-gray-300 rounded-md shadow-sm">
+                         {/* it should be a list */ }
+                        <option className=""> latest</option>
+                        <option className="">oldest</option>
+                    </select>
+                </section>
+
+                <section className="flex flex-col p-2">
+                  <label htmlFor="action">Action Type </label>
+                    <select name="action" id="action" className="p-2 border border-gray-300 rounded-md shadow-sm">
+                        {/* it should be a list */ }
+                        <option className=""> Content Upload</option>
+                        <option className="">User Login</option>
+                    </select>
+
+                </section>
+
+
+
+
+                
+            </header>
+            <section></section>
+            
+            </>
+        )
+}
+
+
+export default AuditLogs;
--- aiStudyCompanion/src/pages/admin/DashboardAdmin.jsx
+++ aiStudyCompanion/src/pages/admin/DashboardAdmin.jsx
@@ -2,6 +2,10 @@
 import { Button } from "../../components/Button";
 import {Dashboard} from "../../components/Dashboard";
 import CardButton from "../../components/cardButton";
+import { useState } from "react";
+import AuditLogs from "./Admincomponents/AuditLogs";
+import ReviewComponent from "./Admincomponents/ReviewComponent";
+
 
 function DashboardAdmin() {
     
@@ -10,65 +14,32 @@ function DashboardAdmin() {
         {id:1015, author: "Norah", date: "2023", select: false }
     ]
 
+    const [selected,setSelectedElements] = useState([])
+
     //const [contentList, setContentList] = useState([]);
     // there should be a get method from the database to get the content panel
     const isClicked=(e) =>{
         const checkedContent = e.target.value 
+        setSelectedElements([...selected,checkedContent]);
+        console.log(selected)
         console.log(e.target.value);
     };
 
+    // there should be a method to remove content
+
+
+    const validateID= (e) => {
+        console.log(e.target.value)
+    };
 
     return (
+        <section className="flex flex-row  ">
+        <ReviewComponent/>
+
+        <AuditLogs/>
+
+                </section>
         
-        <section className="flex flex-col  p-6 m-6 border rounded-lg shadow-lg w-full max-w-md">
-            <h1 className="text-gray-900 font-bold m-2">
-                Review Dashboard Component
-                                                </h1>
-
-        <table>
-            <thead>
-                <tr className="m-4 divide-y-1">
-                    <th className="">Content ID</th>
-                    <th className="">Creator</th>
-                    <th className="">Date Created</th>
-                    <th className="">Select</th>
-                </tr>
-            </thead>
-            <tbody className="py-4 font-bold">
-            
-                
-                {contentList.length > 0 && contentList.map((content) => {
-                    return(
-                    
-                    <tr key={content.id} className=" ">
-
-                        <td>{content.id}</td>
-                        <td>{content.author}</td>
-                        <td>{content.date}</td>
-                        <td>
-                            <input type="checkbox" onClick={isClicked} value={content.id}></input>
-                            
-
-                        </td>
-                    </tr>
-
-                    
-                    )
-
-                })} 
-        </tbody>
-        
-
-           
-        </table>
-        <footer className="m-8">
-
-        <input type="text" placeholder="Search for Content ID" className="mt-4 input border border-gray-300 rounded-md p-4 mb-4 shadow-md"></input>
-        <Button label="Approve" onClick={isClicked} color="green" ></Button>
-
-        </footer>
-
-        </section>
 
         
     );
