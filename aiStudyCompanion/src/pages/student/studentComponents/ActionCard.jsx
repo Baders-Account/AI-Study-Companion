16d3ca7876d1e7c76a152bb45bfa8987fbf9e7b5
Just-Learning-here <108979880+Just-Learning-here@users.noreply.github.com>
2025-11-17 16:51:52 +0300

    Actions Card is Done

--- aiStudyCompanion/src/pages/student/studentComponents/ActionCard.jsx
+++ aiStudyCompanion/src/pages/student/studentComponents/ActionCard.jsx
@@ -3,16 +3,16 @@
 function ActionCard(){
 
     return(
-        <section className="relative flex flex-col  justify-between p-6 mt-15  border rounded-lg shadow-lg w-full min-h-64">
+        <section className="relative flex flex-col  justify-around p-6 mt-15  border rounded-lg shadow-lg w-full min-h-64">
 
-        <h1 className="absolute top-2 left-3 text-2xl font-bold">Action</h1>
+        <h1 className="absolute top-2 left-3 text-2xl font-bold">Actions</h1>
         
-        <section className="flex flex-grow border justify-between  min-h-20">
+        <section className="flex flex-row border justify-around  min-h-20">
         
-        <button>Create Note</button>
-        <button>View Notes</button>
-        <button>AI Assistant</button>
-        <button>View Quizzes</button>
+        <button type="button" className="focus:outline-none text-white bg-gray-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mt-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 cursor-pointer">Create Note</button>
+        <button type="button" className="focus:outline-none text-white bg-gray-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mt-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 cursor-pointer">View Notes</button>
+        <button type="button" className="focus:outline-none text-white bg-gray-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mt-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 cursor-pointer">AI Assistant</button>
+        <button type="button" className="focus:outline-none text-white bg-gray-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mt-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 cursor-pointer">View Quizzes</button>
         </section>
 
         </section>
