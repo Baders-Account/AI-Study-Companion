135a2248544c74d93f2d3155209bcf9ac0f196d4
Just-Learning-here <108979880+Just-Learning-here@users.noreply.github.com>
2025-11-19 22:36:34 +0300

    add an h1 element

--- aiStudyCompanion/src/pages/student/studentComponents/ToDoList.jsx
+++ aiStudyCompanion/src/pages/student/studentComponents/ToDoList.jsx
@@ -41,8 +41,10 @@ function ToDoList(){
             <button onClick={clearAll} type="button" className="focus:outline-none text-white bg-gray-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2  mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 cursor-pointer"> Clear All</button>
             </div>
             
-            <div className="flex flex-col col-start-1 self-start  row-start-1 row-span-2 col-span-8 ">
-
+            <div className="flex flex-col gap-2 col-start-1 self-start  row-start-1 row-span-2 col-span-8 ">
+            <h1 className=" font-bold text-gray-900 dark:text-white mb-4   md:mb-0 lg:text-3xl lg:row-start-1 lg:col-start-1 lg:justify-self-start lg:self-start">
+                To Do List
+            </h1>
 
             <input name="input" type="text" value={inputValue} placeholder=" Type here" className="bg-gray-200 ml-3 p-2 rounded  " onChange={(e) => setInputValue(e.target.value)}></input>
           
